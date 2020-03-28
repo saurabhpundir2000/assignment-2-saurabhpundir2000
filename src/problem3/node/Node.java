55cdc1f22d0a828d99aaf64a86a87672b6049674
saurabhpundir2000 <45098115+saurabhpundir2000@users.noreply.github.com>
2020-03-28 16:13:56 +0530

    Update Node.java

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -7,4 +7,12 @@
 package problem3.node;
 
 public class Node {
+        private int roll;
+
+        public Node(int roll){
+            this.roll=roll;
+        }
+        public int getRoll() {
+            return roll;
+        }
 }
