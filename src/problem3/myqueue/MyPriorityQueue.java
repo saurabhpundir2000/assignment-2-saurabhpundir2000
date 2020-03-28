f0fe19c3b2d7d186b69ffbc941810a286fbc8aba
saurabhpundir2000 <45098115+saurabhpundir2000@users.noreply.github.com>
2020-03-28 16:12:45 +0530

    Update MyPriorityQueue.java

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -6,5 +6,30 @@
  */
 package problem3.myqueue;
 
+import problem3.node.Node;
+
+import java.util.*;
+
 public class MyPriorityQueue {
-}
+        public static Comparator<Node> rollComparator = new Comparator<Node>() {
+
+            @Override
+            public int compare(Node r1, Node r2) {
+                return (int) (r1.getRoll() - r2.getRoll());
+            }
+        };
+        public static void addDataToQueue(Queue<Node> studentPriorityQueue) {
+            for(int i=0; i<10; i++) {
+                int roll = i;
+                studentPriorityQueue.add(new Node(roll));
+            }
+        }
+        public static void pollDataFromQueue(Queue<Node> studentPriorityQueue) {
+            while(true){
+                Node stu = studentPriorityQueue.poll();
+                if(stu == null) break;
+                System.out.println("student roll="+stu.getRoll());
+            }
+        }
+
+    }
