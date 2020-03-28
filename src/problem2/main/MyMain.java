49634b3e241d50b391cbba6b3d2f9653aa0d1115
saurabhpundir2000 <45098115+saurabhpundir2000@users.noreply.github.com>
2020-03-28 17:23:11 +0530

    Update MyMain.java

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -1,11 +1,26 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:15 PM
- */
 package problem2.main;
+import problem2.mybst.MyBinarySearchTree;
 // executable class
-// use problem1.mybst.MyBinarySearchTree class for creating binary tree
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree mytree = new MyBinarySearchTree();
+        mytree.insert(100);
+        mytree.insert(20);
+        mytree.insert(200);
+        mytree.insert(300);
+        mytree.insert(150);
+        mytree.insert(30);
+        mytree.insert(10);
+        System.out.println("PreOrder Traversal : ");
+        mytree.traversePreOrder();
+        System.out.println("InOrder Traversal : ");
+        mytree.traverseInOrder();
+        System.out.println("PostOrder Traversal : ");
+        mytree.traversePostOrder();
+        System.out.println("Condition A : Root element is placed at opposite end," + "\n" +
+                "i.e. in pre order root is the first element where as in post order root is the last element. is TRUE");
+        System.out.println("Condition B :They have common mid point,\n" +
+                "i.e. both the traversal will give same element at the mid position for odd number of nodes. is TRUE");
+    }
 }
