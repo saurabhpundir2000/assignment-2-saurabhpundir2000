bce14ab2c3329d698f652bfe80b597d917262aab
saurabhpundir2000 <45098115+saurabhpundir2000@users.noreply.github.com>
2020-03-28 17:28:52 +0530

    Update Student.java

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -1,10 +1,57 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:06 PM
- */
 package problem5.student;
 // to store student information and properties
 public class Student {
+    private int rollNo;
+    private String name;
+    private int backLogCounter;
+    private int appearingCount;
+
+    public Student(int rollNo, String name) {
+        this.rollNo = rollNo;
+        this.name = name;
+    }
+
+    public Student(int rollNo, String name, int backLogCounter, int appearingCount) {
+        this.rollNo = rollNo;
+        this.name = name;
+        this.backLogCounter = backLogCounter;
+        this.appearingCount = appearingCount;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name + " #" + rollNo;
+    }
+
+    public int getBackLogCounter() {
+        return backLogCounter;
+    }
+
+    public void setBackLogCounter(int backLogCounter) {
+        this.backLogCounter = backLogCounter;
+    }
+
+    public int getAppearingCount() {
+        return appearingCount;
+    }
+
+    public void setAppearingCount(int appearingCount) {
+        this.appearingCount = appearingCount;
+    }
 }
