3d73575a1ebb324090e52f693c1e3decd0fcac6d
saurabhpundir2000 <45098115+saurabhpundir2000@users.noreply.github.com>
2020-03-28 17:20:02 +0530

    Update MyMain.java

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -1,11 +1,24 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:17 PM
- */
 package problem1.main;
-// executable class
+
+import problem1.mybst.MyBinarySearchTree;
 
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree mytree = new MyBinarySearchTree();
+        mytree.insert(50);
+        mytree.insert(30);
+        mytree.insert(20);
+        mytree.insert(40);
+        mytree.insert(70);
+        mytree.insert(60);
+        mytree.insert(80);
+        mytree.insert(90);
+        mytree.insert(10);
+        System.out.println("PreOrder Traversal : ");
+        mytree.traversePreOrder();
+        System.out.println("Left children Only : ");
+        //traverseLeft() methods prints all left children as well as returns the no of nodes not having left a child
+        System.out.println("No of nodes not having Left Subchild: " + mytree.traverseLeft());
+
+    }
 }
